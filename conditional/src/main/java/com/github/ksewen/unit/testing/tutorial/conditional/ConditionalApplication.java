b41080477cb4762612b44f84d0517d0cef86938f
ksewen <261517946@qq.com>
2024-01-07 17:17:22 +0300

    init project

---
+++ conditional/src/main/java/com/github/ksewen/unit/testing/tutorial/conditional/ConditionalApplication.java
@@ -0,0 +1,12 @@
+package com.github.ksewen.unit.testing.tutorial.conditional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConditionalApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ConditionalApplication.class, args);
+  }
+}
---
+++ conditional/src/test/java/com/github/ksewen/unit/testing/tutorial/conditional/ConditionalApplicationTests.java
@@ -0,0 +1,11 @@
+package com.github.ksewen.unit.testing.tutorial.conditional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConditionalApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
